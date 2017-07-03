8fa96a88fe48e6bfc2565f7002c39d22c279ff63
John Duggan <JLDuggan87@gmail.com>
2017-07-03 15:39:18 -0400

    add battles show container

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import NavBar from './components/NavBar';
+import BattlesContainer from './containers/BattlesContainer'
 
 
 const App = props => {
   return(
     <Router history={browserHistory}>
-      <Route path='/' component={NavBar}/>
+      <Route path='/' component={NavBar}>
+        <IndexRoute component={BattlesContainer}/>
+      </Route>
     </Router>
   )
 }
